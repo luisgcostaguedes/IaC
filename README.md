c8d03dfc8e56fe3180441880c85f051b76cefecd
luisgcostaguedes <luisgcostaguedes@gmail.com>
2023-12-04 15:39:09 -0300

    up para o note

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # IaC
+
+Tudo o que foi ensinado no curso da Alura de IaC
--- main.tf
+++ main.tf
@@ -16,9 +16,15 @@ provider "aws" {
 resource "aws_instance" "app_server" {
   ami           = "ami-0efcece6bed30fd98"
   instance_type = "t2.micro"
-  key_name = "iac-luis"
+  key_name = "iac-luis" //nome do .pem que foi criado
+  # user_data = <<-EOF
+  #                  #!/bin/bash
+  #                  cd /home/ubuntu
+  #                  echo "<h1>Feito no terraform</h1>" > index.html
+  #                  nohup busybox httpd -f -p 8080 &
+  #                EOF
 
   tags = {
-    Name = "MyFirstInstance"
+    Name = "AWS ansible python"
   }
 }
